c95b90dfc8a097407dc06c4acd372b5b8a124c30
Tarandeep-singh1 <ts9559418@gmail.com>
2024-10-20 15:03:16 +0530

    Create data.js

---
+++ Backend/config/data.js
@@ -0,0 +1,9 @@
+const data=[
+    { teacherName: 'Teacher A', question: 'Question 1', averageRating: 4.5 },
+    { teacherName: 'Teacher A', question: 'Question 2', averageRating: 3.8 },
+    // ... other data points for Teacher A
+    { teacherName: 'Teacher B', question: 'Question 1', averageRating: 4.2 },
+    { teacherName: 'Teacher B', question: 'Question 2', averageRating: 4.0 },
+    // ... other data points for Teacher B
+]
+module.exports=data;
